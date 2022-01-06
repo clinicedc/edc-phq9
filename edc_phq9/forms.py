ff938c405f320f2ddc34a92c04596ada5b201491
erikvw <ew2789@gmail.com>
2022-01-06 05:49:24 -0600

    separate as mixin

--- edc_phq9/forms.py
+++ edc_phq9/forms.py
@@ -11,13 +11,18 @@ from edc_phq9.fieldsets import get_phq9_fields
 from .utils import get_phq9_model_cls
 
 
-class Phq9FormValidator(CrfFormValidatorMixin, FormValidator):
-    def clean(self):
+class Phq9FormValidatorMixin:
+    def clean_phq9(self):
         if getattr(settings, "EDC_PHQ9_MODEL_AT_BASELINE_ONLY", True):
             raise_if_not_baseline(self.cleaned_data.get("subject_visit"))
         self.required_if(NO, field="ph9_performed", field_required="ph9_not_performed_reason")
         for fld in get_phq9_fields():
             self.applicable_if(YES, field="ph9_performed", field_applicable=fld)
+
+
+class Phq9FormValidator(Phq9FormValidatorMixin, CrfFormValidatorMixin, FormValidator):
+    def clean(self):
+        self.clean_phq9()
 
 
 class Phq9Form(FormValidatorMixin, forms.ModelForm):
