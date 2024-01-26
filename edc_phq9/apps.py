816d31b66f947a83ad5030361aadfd57a5d8fc19
erikvw <ew2789@gmail.com>
2024-01-26 12:41:10 -0600

    fix tests

--- edc_phq9/apps.py
+++ edc_phq9/apps.py
@@ -1,5 +1,3 @@
-import sys
-
 from django.apps import AppConfig as DjangoAppConfig
 
 
@@ -8,7 +6,3 @@ class AppConfig(DjangoAppConfig):
     verbose_name = "Edc PHQ9"
     has_exportable_data = True
     include_in_administration_section = True
-
-    def ready(self):
-        sys.stdout.write("Loading {} ...\n".format(self.verbose_name))
-        sys.stdout.write(" Done loading {}.\n".format(self.verbose_name))
